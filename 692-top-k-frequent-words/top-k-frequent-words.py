b29cfd82ab8f50b39cd402a275b43966beee3dff
Benedict Nursalim <93800156+benedictnurs@users.noreply.github.com>
2024-08-22 01:11:59 -0700

    Time: 62 ms (29.45%) | Memory: 16.7 MB (32.85%) - LeetSync

--- 692-top-k-frequent-words/top-k-frequent-words.py
+++ 692-top-k-frequent-words/top-k-frequent-words.py
@@ -3,13 +3,13 @@ class Solution:
         words = Counter(words)
         heap = []
         for word,count in words.items():
-            heapq.heappush(heap,(-count,word))
+            heappush(heap,(-count,word))
         
 
         ans = []
 
         while k:
-            val = heapq.heappop(heap)
+            val = heappop(heap)
             ans.append(val[1])
             k-=1
         
