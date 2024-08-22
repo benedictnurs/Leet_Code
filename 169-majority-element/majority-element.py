cfbf703b1f1f34fa9360bfdab5ba16626ea4bb86
Benedict Nursalim <93800156+benedictnurs@users.noreply.github.com>
2024-08-21 23:33:26 -0700

    Time: 167 ms (71.82%) | Memory: 18 MB (20.05%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,8 +1,6 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        hm = {}
         ans = [float(-inf),0]
         for i in set(nums):
-            hm[nums.count(i)] = i
             ans = max(ans,[nums.count(i),i])
         return(ans[1])
