3844385ccf5f62b7982129f0f42ba53281987fc1
Benedict Nursalim <93800156+benedictnurs@users.noreply.github.com>
2024-08-24 03:33:25 -0700

    Time: 52 ms (95.06%) | Memory: 16.7 MB (18.62%) - LeetSync

--- 2594-count-pairs-of-similar-strings/count-pairs-of-similar-strings.py
+++ 2594-count-pairs-of-similar-strings/count-pairs-of-similar-strings.py
@@ -5,13 +5,10 @@ class Solution:
 
         for i in words:
             word = "".join(sorted(set(i)))
-            print(word)
             if word in hm:
                 count += hm[word]
                 hm[word] += 1
-                print(count)
             else:
                 hm[word] = 1
-        print(hm)
 
         return count
