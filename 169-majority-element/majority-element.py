814cf2cef8fff460462760af3bebbb0cdc92207f
Benedict Nursalim <93800156+benedictnurs@users.noreply.github.com>
2024-08-25 20:25:34 -0700

    Time: 169 ms (63.04%) | Memory: 18.2 MB (20.76%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,9 +1,7 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         nums = Counter(nums)
-        print(nums)
         heap = []
         for k,v in nums.items():
             heappush(heap,(-v,k))
-        
         return (heappop(heap)[1])
