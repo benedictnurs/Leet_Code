1d15ded99872a3ad7dfe6d2aa68f979161d75dc7
Benedict Nursalim <93800156+benedictnurs@users.noreply.github.com>
2024-08-25 20:12:37 -0700

    Time: 131 ms (19.06%) | Memory: 20.3 MB (93.88%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -3,11 +3,14 @@ class Solution:
         intervals.sort()
         sol = []
         l = 0
-
+     
         for r in range(1, len(intervals)):
+            #If the ending of the left greater than start of the right
+            #Ending of the left will be the maximum out of the endings
             if intervals[r][0] <= intervals[l][1]:
                 intervals[l][1] = max(intervals[l][1], intervals[r][1])
             else:
+            #Else just append and move the left to the current index
                 sol.append(intervals[l])
                 l = r
         
