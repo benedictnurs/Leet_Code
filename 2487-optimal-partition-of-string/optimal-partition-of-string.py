f5af92c4ca6b401d00a9d52d7f083c12ed946d75
Benedict Nursalim <93800156+benedictnurs@users.noreply.github.com>
2024-08-28 03:38:36 -0700

    Time: 94 ms (48.48%) | Memory: 17.4 MB (58.80%) - LeetSync

--- 2487-optimal-partition-of-string/optimal-partition-of-string.py
+++ 2487-optimal-partition-of-string/optimal-partition-of-string.py
@@ -2,11 +2,12 @@ class Solution:
     def partitionString(self, s: str) -> int:
         current = []
         count = 0
+        
         for i in s:
             if i not in current:
                 current.append(i)
             else:
-                count+=1
+                count += 1
                 current = [i]
         
-        return (count) + 1
+        return count + 1
