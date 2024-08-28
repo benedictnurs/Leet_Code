56f854717cad4e8f443c1968bd14de2e0baa1f39
Benedict Nursalim <93800156+benedictnurs@users.noreply.github.com>
2024-08-28 02:05:58 -0700

    Time: 351 ms (5.09%) | Memory: 18.1 MB (6.01%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.py
+++ 485-max-consecutive-ones/max-consecutive-ones.py
@@ -6,10 +6,8 @@ class Solution:
         for i in range(len(nums)):
             if nums[i] == 1:
                 count+=1
-                print(count)
                 sol = max(sol,count)
             else:
                 count = 0
         
-        print(sol)
         return sol
