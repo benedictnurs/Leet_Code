5ee28bd8fa830c5270a5f2e0015395ee606772a0
Benedict Nursalim <93800156+benedictnurs@users.noreply.github.com>
2024-08-31 00:02:28 -0700

    Time: 42 ms (22.98%) | Memory: 16.6 MB (50.90%) - LeetSync

---
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -0,0 +1,7 @@
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        if str1+str2 == str2+str1:
+            g = (gcd(len(str1),len(str2)))
+            return("".join(list(str1+str2)[:g]))
+        
+        return ""
