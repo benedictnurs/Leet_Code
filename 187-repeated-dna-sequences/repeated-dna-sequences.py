06410c33704156af1c3a766b03b012d0695ceeb6
Benedict Nursalim <93800156+benedictnurs@users.noreply.github.com>
2024-08-31 03:37:14 -0700

    Time: 62 ms (46.28%) | Memory: 27.9 MB (84.30%) - LeetSync

--- 187-repeated-dna-sequences/repeated-dna-sequences.py
+++ 187-repeated-dna-sequences/repeated-dna-sequences.py
@@ -6,16 +6,9 @@ class Solution:
         ans = []
         if k > len(s):
             return []
-            
-        for i in range(k):
-            curr.append(s[i])
-            dna = "".join(curr)
-        hm[dna] = 1
         
-        for i in range(k, len(s)):
-            curr.pop(0)
-            curr.append(s[i])
-            dna = "".join(curr)
+        for i in range(len(s)-k+1):
+            dna = s[i:i+k]
             if dna not in hm:
                 hm[dna] = 1
             else:
