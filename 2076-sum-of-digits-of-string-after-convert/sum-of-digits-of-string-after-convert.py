919228115bef0b2970c7500afc7a404b4e42ad55
Benedict Nursalim <93800156+benedictnurs@users.noreply.github.com>
2024-09-03 01:03:55 -0700

    Time: 41 ms (35.09%) | Memory: 16.4 MB (73.84%) - LeetSync

--- 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.py
+++ 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.py
@@ -1,8 +1,6 @@
 class Solution:
     def getLucky(self, s: str, k: int) -> int:
-        sol = ""
-        for i in s:
-            sol+=(str((ord(i))-96))
+        sol = "".join(str(ord(i) - 96) for i in s)
 
         while k:
             a = sum([int(i) for i in sol])
