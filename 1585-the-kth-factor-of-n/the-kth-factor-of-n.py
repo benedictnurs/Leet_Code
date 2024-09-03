5c6a8a40d57ee5031dca627c7ae044af451f9083
Benedict Nursalim <93800156+benedictnurs@users.noreply.github.com>
2024-09-03 02:30:39 -0700

    Time: 31 ms (87.08%) | Memory: 16.4 MB (84.74%) - LeetSync

--- 1585-the-kth-factor-of-n/the-kth-factor-of-n.py
+++ 1585-the-kth-factor-of-n/the-kth-factor-of-n.py
@@ -4,7 +4,7 @@ class Solution:
         count = 0
         for i in range(1, n+1):
             if n % i == 0:
-                count+=1
+                count +=1
                 if count == k:
                     return (i)
         return -1
