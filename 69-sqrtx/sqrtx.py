2ccadcd8b8f99ceeddcb63618f23688a23eb9af3
Benedict Nursalim <93800156+benedictnurs@users.noreply.github.com>
2024-09-07 00:25:19 -0700

    Time: 44 ms (36.49%) | Memory: 16.5 MB (21.26%) - LeetSync

---
+++ 69-sqrtx/sqrtx.py
@@ -0,0 +1,15 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        l = 0
+        r = x
+
+        while l <= r:
+            mid = (l + r) // 2
+            if mid * mid < x:
+                l = mid + 1
+            elif mid * mid > x:
+                r = mid - 1
+            else:
+                return mid
+        
+        return r
