fc4f7aa6d6ecf823f049d400d2ecff52c6206d20
Benedict Nursalim <93800156+benedictnurs@users.noreply.github.com>
2024-09-08 02:43:55 -0700

    Time: 46 ms (100.00%) | Memory: 16.5 MB (100.00%) - LeetSync

--- 3567-convert-date-to-binary/convert-date-to-binary.py
+++ 3567-convert-date-to-binary/convert-date-to-binary.py
@@ -3,7 +3,5 @@ class Solution:
         lst = (date.split("-"))
         ans = ""
         for i in lst:
-            print("{0:b}".format(int(i)))
             ans += "{0:b}".format(int(i)) + "-"
-
         return(ans[:-1])
