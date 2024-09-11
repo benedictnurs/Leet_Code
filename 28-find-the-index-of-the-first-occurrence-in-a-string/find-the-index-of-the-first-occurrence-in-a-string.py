ba96489fcaa86e5de163bebd1b283a07519f2a4f
Benedict Nursalim <93800156+benedictnurs@users.noreply.github.com>
2024-09-11 02:24:36 -0700

    Time: 31 ms (80.14%) | Memory: 16.4 MB (93.34%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
@@ -3,7 +3,6 @@ class Solution:
         ans = 0
         k = len(needle)
         for i in range(len(haystack)-k+1):
-            print(haystack[i:i+k])
             if haystack[i:i+k] == needle:
                 return i
         return -1
