f1360f8c338a2a4c5285579f47ed1f853d881e55
Benedict Nursalim <93800156+benedictnurs@users.noreply.github.com>
2024-09-12 00:52:01 -0700

    Time: 289 ms (12.34%) | Memory: 18.3 MB (28.78%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.py
+++ 658-find-k-closest-elements/find-k-closest-elements.py
@@ -2,13 +2,15 @@ class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
         heap = []
         sol = []
+        heapify(sol)
 
         for i in (arr):
             heappush(heap,(abs(x - i), i))
         
         while k:
             h = heappop(heap)
-            heappush(sol, h[1])
+            sol.append(h[1])
             k-=1
         
-        return sorted(sol)
+        sol.sort()
+        return sol
