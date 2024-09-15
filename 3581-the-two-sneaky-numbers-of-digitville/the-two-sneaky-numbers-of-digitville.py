6bce9649b6bc5f9be0d61d38a6480c47df8e6699
Benedict Nursalim <93800156+benedictnurs@users.noreply.github.com>
2024-09-15 03:25:23 -0700

    Time: 39 ms (100.00%) | Memory: 16.5 MB (100.00%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.py
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.py
@@ -2,10 +2,8 @@ class Solution:
     def getSneakyNumbers(self, nums: List[int]) -> List[int]:
         nums.sort()
         ans = []
-
         for i in range(len(nums)-2+1):
             temp = (nums[i:i+2])
             if temp[0] == temp[1]:
                 ans.append(nums[i])
-        print(ans)
         return ans
