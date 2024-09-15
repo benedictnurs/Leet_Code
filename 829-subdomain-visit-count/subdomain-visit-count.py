9a11702f5250a7c0f4b0b80e54761436001b398f
Benedict Nursalim <93800156+benedictnurs@users.noreply.github.com>
2024-09-15 01:53:33 -0700

    Time: 52 ms (58.77%) | Memory: 16.5 MB (64.05%) - LeetSync

---
+++ 829-subdomain-visit-count/subdomain-visit-count.py
@@ -0,0 +1,18 @@
+class Solution:
+    def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
+        hm = {}
+        sol = []
+
+        for domain in cpdomains:
+            domainList = domain.split()[1].split(".")
+            for index in range(len(domainList)):
+                subdomain = ".".join(domainList[index:len(domainList)])
+                if subdomain in hm:
+                    hm[subdomain] += int(domain.split()[0])
+                else:
+                    hm[subdomain] = int(domain.split()[0])
+
+        for domain, visits in hm.items():
+            sol.append(str(visits) + " " + domain)
+ 
+        return sol
