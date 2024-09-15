86b70733312e9e078d444f5f22b24faf00763ef0
Benedict Nursalim <93800156+benedictnurs@users.noreply.github.com>
2024-09-15 04:00:13 -0700

    Time: 728 ms (25.30%) | Memory: 27.3 MB (40.96%) - LeetSync

---
+++ 1945-finding-the-users-active-minutes/finding-the-users-active-minutes.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findingUsersActiveMinutes(self, logs: List[List[int]], k: int) -> List[int]:
+        #we want to create an array and find how many users were active 
+        #at that time the index represents the unique times they visited
+        #How many people visited once?
+        hm = {}
+        lst = k*[0]
+        for log in logs:
+            user_id = log[0]
+            minute = log[1]
+            if user_id in hm:
+                hm[user_id].add(minute)
+            else:
+                hm[user_id] = {minute}   
+        
+        for log in hm:
+            location = len(hm[log])-1
+            lst[location]+=1
+
+        print(lst)
+        return lst
