875fec88556da05c10414cfc80556fd57934092b
Benedict Nursalim <93800156+benedictnurs@users.noreply.github.com>
2024-09-19 18:30:57 -0700

    Time: 59 ms (22.28%) | Memory: 16.5 MB (35.17%) - LeetSync

---
+++ 2977-check-if-a-string-is-an-acronym-of-words/check-if-a-string-is-an-acronym-of-words.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isAcronym(self, words: List[str], s: str) -> bool:
+        comp = ""
+
+        for word in words:
+            first = list(word)[0]
+            comp += first
+        print(comp)
+        return comp == s
