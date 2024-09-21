c7df467267959b4e07d3f47bdcc615cae8449e09
Benedict Nursalim <93800156+benedictnurs@users.noreply.github.com>
2024-09-20 23:53:54 -0700

    Time: 25 ms (97.27%) | Memory: 16.5 MB (37.35%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.py
+++ 807-custom-sort-string/custom-sort-string.py
@@ -1,19 +1,17 @@
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
-        orderLst = []
-        sett = set(list(s))
-        lst = list(s)
-        sol = ""
-        
-        for i in range(len(order)):
-            orderLst.append(order[i])
+        # Count the frequency of each character in s
+        s_count = Counter(s)
+        result = []
 
-        for i in orderLst:
-            if i in sett:
-                sol += i*s.count(i)
-                sett.remove(i)
+        # Add characters from `order` in the specified order
+        for char in order:
+            if char in s_count:
+                result.append(char * s_count[char])
+                del s_count[char]
 
-        for i in sorted(sett):
-            sol += i*lst.count(i)
+        # Add remaining characters that were not in `order`
+        for char, count in s_count.items():
+            result.append(char * count)
 
-        return sol
+        return ''.join(result)
