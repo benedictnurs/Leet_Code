91d518536e7882fab47aedd132197da9e86451ea
Benedict Nursalim <93800156+benedictnurs@users.noreply.github.com>
2024-09-21 02:47:28 -0700

    Time: 37 ms (46.48%) | Memory: 16.6 MB (26.67%) - LeetSync

--- 2580-circular-sentence/circular-sentence.py
+++ 2580-circular-sentence/circular-sentence.py
@@ -5,10 +5,10 @@ class Solution:
             print(lst)
             return lst[0][-1] == lst[0][0]
 
-        l = 0
         if lst[-1][-1] != lst[0][0]:
             return False
-
+ 
+        l = 0
         for r in range(1, len(lst)):
             print(lst[l][-1] ,lst[r][0])
             if lst[l][-1] == lst[r][0]:
