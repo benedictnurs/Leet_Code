d79fba72aedf185f3e5ca9b9ab4f50f1d7a7f306
Benedict Nursalim <93800156+benedictnurs@users.noreply.github.com>
2024-09-20 18:51:18 -0700

    Time: 131 ms (27.13%) | Memory: 16.6 MB (5.86%) - LeetSync

---
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.py
@@ -0,0 +1,27 @@
+class Solution:
+    def findThePrefixCommonArray(self, A: List[int], B: List[int]) -> List[int]:
+        total = []
+        hm = {}
+
+        sol = []
+
+        for i in range(len(A)):
+            if A[i] not in hm:
+                hm[A[i]] = 1
+            elif A[i] in hm:
+                hm[A[i]] += 1
+ 
+            if B[i] not in hm:
+                hm[B[i]] = 1  
+            elif B[i] in hm:
+                hm[B[i]] += 1
+            
+            count = 0
+            for i in hm.values():
+                if i == 2:
+                    count += 1
+            total.append(count)
+            
+        print(total)
+        return total         
+
