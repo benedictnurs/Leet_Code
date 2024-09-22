0e5986ecf5881b6e819eaf8f3bc7b857a36fb70d
Benedict Nursalim <93800156+benedictnurs@users.noreply.github.com>
2024-09-22 02:59:20 -0700

    Time: 50 ms (83.49%) | Memory: 16.6 MB (72.73%) - LeetSync

--- 3540-hash-divided-string/hash-divided-string.py
+++ 3540-hash-divided-string/hash-divided-string.py
@@ -2,9 +2,7 @@ class Solution:
     def stringHash(self, s: str, k: int) -> str:
         lst = []
         sol = ""
-        alph = {}
-
-
+        
         for i in range(0, len(s), k):
             lst.append(s[i:i+k])
 
@@ -14,7 +12,6 @@ class Solution:
                 numVal += (ord(letter)-97)
             sol += chr((numVal%26)+97)
             
-            print(numVal%26, numVal)
         return sol
 
 
