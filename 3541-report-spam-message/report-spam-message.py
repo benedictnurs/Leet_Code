49424eeb0c676b1fca9ee50d08ba369671f44054
Benedict Nursalim <93800156+benedictnurs@users.noreply.github.com>
2024-09-21 22:18:06 -0700

    Time: 1346 ms (33.33%) | Memory: 55.1 MB (33.33%) - LeetSync

---
+++ 3541-report-spam-message/report-spam-message.py
@@ -0,0 +1,6 @@
+class Solution:
+    def reportSpam(self, message: List[str], bannedWords: List[str]) -> bool:
+        s = set(bannedWords)
+
+        
+        return sum([i in s for i in message]) >= 2
