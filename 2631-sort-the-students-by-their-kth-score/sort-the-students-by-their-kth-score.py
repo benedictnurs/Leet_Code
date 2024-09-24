07c32522fe45f6f529869bfbc2e11439c5dbf193
Ben N <93800156+benedictnurs@users.noreply.github.com>
2024-09-24 04:06:11 -0700

    Time: 329 ms (28.17%) | Memory: 22.7 MB (16.28%) - LeetSync

--- 2631-sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.py
+++ 2631-sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.py
@@ -1,14 +1,3 @@
 class Solution:
-    def sortTheStudents(self, score: List[List[int]], k: int) -> List[List[int]]:
-        hm = {}
-        sol = []
-        for row in range(len(score)):
-            column = []
-            for col in range(len(score[0])):
-                column.append(score[row][col])
-            hm[row] = (column[k], column)
-
-        for ranking, row in sorted(hm.values(), key=lambda x: x[0], reverse = True):
-            sol.append(row)
-        
-        return sol
+    def sortTheStudents(self, score: List[List[int]], k: int) -> List[List[int]]:        
+        return sorted(score, key=lambda x: x[k], reverse = True)
