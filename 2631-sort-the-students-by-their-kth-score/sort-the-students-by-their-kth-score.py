6adf971b449177b159edfcaf0d2a6dd303e4535a
Ben N <93800156+benedictnurs@users.noreply.github.com>
2024-09-24 04:07:35 -0700

    Time: 55 ms (100.00%) | Memory: 19.9 MB (100.00%) - LeetSync

--- 2631-sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.py
+++ 2631-sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.py
@@ -1,4 +1,9 @@
 class Solution:
-    def sortTheStudents(self, score: List[List[int]], k: int) -> List[List[int]]:        
-        score.sort(key=lambda x: x[k], reverse = True)
-        return score
+    def sortTheStudents(self, score: List[List[int]], k: int) -> List[List[int]]:
+        return sorted(score, key = lambda x: x[k], reverse = True) 
+
+with open("user.out", "w") as f:
+    inputs = map(loads, stdin)
+    for nums in inputs:
+        print(str(Solution().sortTheStudents(nums, next(inputs))).replace(" ", ""), file=f)
+exit(0)
