f2c976745d6672ece6e7f4ec19cc937dbbbb2c8e
Ben N <93800156+benedictnurs@users.noreply.github.com>
2024-09-24 19:30:49 -0700

    Time: 113 ms (53.90%) | Memory: 16.8 MB (60.87%) - LeetSync

--- 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.py
+++ 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.py
@@ -1,9 +1,7 @@
 class Solution:
     def luckyNumbers(self, matrix: List[List[int]]) -> List[int]:
-        # Step 1: Find the minimum in each row
         row_mins = {min(row) for row in matrix}
         
-        # Step 2: Find the maximum in each column
         col_maxs = set()
         for c in range(len(matrix[0])):
             maximum = float("-inf")
@@ -11,5 +9,4 @@ class Solution:
                 maximum = max(maximum, matrix[r][c])
             col_maxs.add(maximum)
         
-        # Step 3: Return the intersection of row mins and column maxs
         return list(row_mins & col_maxs)
