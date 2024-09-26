8eb6f7e61d528271d3a0ed81680ac6267a72f70b
Ben N <93800156+benedictnurs@users.noreply.github.com>
2024-09-26 12:21:06 -0700

    Time: 37 ms (80.79%) | Memory: 16.5 MB (72.97%) - LeetSync

--- 768-partition-labels/partition-labels.py
+++ 768-partition-labels/partition-labels.py
@@ -8,10 +8,12 @@ class Solution:
         
         maxs = 0
         count = 0
+
         for i in range(len(s)):
             maxs = max(maxs, hm[s[i]])
             count += 1
             if i == maxs:
                 sol.append(count)
                 count = 0
+                
         return sol
