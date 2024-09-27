40c58fd7585c6c244c38a925d618226eccb9609c
Ben N <93800156+benedictnurs@users.noreply.github.com>
2024-09-27 16:49:45 -0700

    Time: 1246 ms (27.02%) | Memory: 35 MB (49.86%) - LeetSync

---
+++ 3420-find-occurrences-of-an-element-in-an-array/find-occurrences-of-an-element-in-an-array.py
@@ -0,0 +1,20 @@
+class Solution:
+    def occurrencesOfElement(self, nums: List[int], queries: List[int], x: int) -> List[int]:
+        hm = {}
+        sol = []
+        seenOrder = 1
+
+        for i in range(len(nums)):
+            if nums[i] == x:
+                hm[seenOrder] = i
+                seenOrder += 1
+
+
+        for q in queries:
+            if q in hm:
+                sol.append(hm[q])
+            else:
+                sol.append(-1)
+        print(sol)
+        return sol
+
