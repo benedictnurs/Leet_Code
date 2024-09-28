5a8ccbd556d43c0d0038ffbdfcabad001167e099
Ben N <93800156+benedictnurs@users.noreply.github.com>
2024-09-28 16:01:08 -0700

    Time: 1392 ms (71.32%) | Memory: 73 MB (7.08%) - LeetSync

---
+++ 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
+        flattened_list = list(chain(*matches))
+
+        peeps = set(flattened_list)
+        hm = {}
+        noloss = []
+        oneloss = []
+
+        for i in matches:
+            lose = i[1]
+            if lose not in hm:
+                hm[lose] = 1
+            else:
+                hm[lose] += 1
+
+        for i in sorted(peeps):
+            if i not in hm:
+                noloss.append(i)
+            if i in hm and hm[i] == 1:
+                oneloss.append(i)
+        print([(noloss),(oneloss)])
+        return ([(noloss),(oneloss)])
