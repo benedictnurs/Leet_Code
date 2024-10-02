0724f5516161bb99e86051d6851079789391d2a7
Ben N <93800156+benedictnurs@users.noreply.github.com>
2024-10-02 12:36:03 -0700

    Time: 495 ms (74.51%) | Memory: 54 MB (35.99%) - LeetSync

---
+++ 2232-adding-spaces-to-a-string/adding-spaces-to-a-string.py
@@ -0,0 +1,10 @@
+class Solution:
+    def addSpaces(self, s: str, spaces: List[int]) -> str:
+        sol = ""
+        l = 0
+
+        for r in spaces:
+            sol+=(s[l:r]+" ") 
+            l = r
+
+        return (sol + s[l:])
