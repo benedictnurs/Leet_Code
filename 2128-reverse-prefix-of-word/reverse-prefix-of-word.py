d02bcbe8cc0b827f410891c33da6a7abc871a27a
Ben N <93800156+benedictnurs@users.noreply.github.com>
2024-10-02 23:48:01 -0700

    Time: 37 ms (40.90%) | Memory: 16.5 MB (30.35%) - LeetSync

--- 2128-reverse-prefix-of-word/reverse-prefix-of-word.py
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.py
@@ -6,11 +6,12 @@ class Solution:
         for r in range(len(word)):
             if word[r] == ch and end == 0:
                 end = r + 1
-
+                break
+                
         for l in range(end):
             stack.append(word[l])
         
         for i in range(len(stack)):
             sol += stack.pop()
 
-        return (sol + word[end:])
+        return sol + word[end:]
