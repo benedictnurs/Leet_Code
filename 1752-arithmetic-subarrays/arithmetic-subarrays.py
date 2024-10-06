ecfd0ec47715bb76a6a6e5e96fbfc52b4819a034
Ben N <93800156+benedictnurs@users.noreply.github.com>
2024-10-06 01:02:06 -0700

    Time: 153 ms (43.14%) | Memory: 16.7 MB (82.15%) - LeetSync

---
+++ 1752-arithmetic-subarrays/arithmetic-subarrays.py
@@ -0,0 +1,18 @@
+class Solution:
+    def checkArithmeticSubarrays(self, nums: List[int], l: List[int], r: List[int]) -> List[bool]:
+        sol = [True for i in range(len(l))]
+
+        for index in range(len(l)):
+            start = l[index]
+            end = r[index] + 1
+            lst = sorted(nums[start:end])
+            diff = lst[1]-lst[0]
+
+            for i in range(len(lst)-1):
+                if (lst[i+1]-lst[i]) != diff:
+                    sol[index] = False
+                    break
+                
+                
+        return (sol) 
+        
