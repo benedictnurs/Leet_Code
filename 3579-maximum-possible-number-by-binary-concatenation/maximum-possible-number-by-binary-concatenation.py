c01a1bba6a6301c355fd2621d2adbc5a200a77a0
Ben N <93800156+benedictnurs@users.noreply.github.com>
2024-10-07 13:33:15 -0700

    Time: 44 ms (27.08%) | Memory: 16.6 MB (17.82%) - LeetSync

--- 3579-maximum-possible-number-by-binary-concatenation/maximum-possible-number-by-binary-concatenation.py
+++ 3579-maximum-possible-number-by-binary-concatenation/maximum-possible-number-by-binary-concatenation.py
@@ -2,6 +2,7 @@ class Solution:
     def maxGoodNumber(self, nums: List[int]) -> int:
         lst = ["{0:b}".format(i) for i in nums]
         sol = float(-inf)
+
         for combo in permutations(lst):
             concatenated = ''.join(combo)
             sol = max(sol, int(concatenated, 2))
