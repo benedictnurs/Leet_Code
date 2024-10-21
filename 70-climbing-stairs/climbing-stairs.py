bb87b33f68eea9a2516070ecaa8494a92d369438
Ben N <93800156+benedictnurs@users.noreply.github.com>
2024-10-21 12:00:08 -0700

    Time: 0 ms (100.00%) | Memory: 16.6 MB (6.21%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,16 +1,21 @@
+"""
+DP
+
+"""
+
 class Solution:
     def climbStairs(self, n: int) -> int:
         if n == 1:
             return 1
         if n == 2:
             return 2
-
-        sol = [0] * (n+2)
-        sol[0] = 0
-        sol[1] = 1
+        
+        dp = [0] * (n + 2)
+        dp[0] = 0
+        dp[1] = 1
 
         for i in range(2, n+2):
-            sol[i] = sol[i-1] + sol[i-2]
+            dp[i] = dp[i-1] + dp[i-2]
         
-        print(sol) 
-        return sol[-1]
+        return dp[-1]
+            
