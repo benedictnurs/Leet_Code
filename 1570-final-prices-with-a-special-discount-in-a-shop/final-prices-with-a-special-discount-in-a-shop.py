2828cb4ff3fa24ba2f34ad0974d63243fea090f4
Ben N <93800156+benedictnurs@users.noreply.github.com>
2024-10-21 11:47:50 -0700

    Time: 2 ms (99.33%) | Memory: 16.5 MB (84.41%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.py
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.py
@@ -1,3 +1,10 @@
+"""
+Stack
+Use a copy becase we want to keep the orginal elements
+Use an index based stack because it wants us to hold a minimum index
+
+"""
+
 class Solution:
     def finalPrices(self, prices: List[int]) -> List[int]:
         initial = prices[:]
@@ -7,6 +14,5 @@ class Solution:
             while stack and prices[stack[-1]] >= prices[i]:
                 initial[stack.pop()] -= prices[i]
             stack.append(i)
-        
-        return (initial)
-        
+
+        return initial
