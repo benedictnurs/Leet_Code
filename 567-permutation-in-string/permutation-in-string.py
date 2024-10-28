bc310a0956405617084503feedb01e2b4f102216
Ben N <93800156+benedictnurs@users.noreply.github.com>
2024-10-27 20:43:35 -0700

    Time: 1238 ms (15.15%) | Memory: 16.8 MB (7.74%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,7 +1,6 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         window = Counter(list(s1))
-        print(window)
         k = len(s1)
 
         for i in range(len(s2)-k+1):
