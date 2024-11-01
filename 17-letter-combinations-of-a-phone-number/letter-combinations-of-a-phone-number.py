47f4f5cbd81fc0b8524f46f557023e8ad53beb21
Ben N <93800156+benedictnurs@users.noreply.github.com>
2024-11-01 03:52:12 -0700

    Time: 0 ms (100.00%) | Memory: 16.5 MB (91.68%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -0,0 +1,25 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if digits == "":
+            return []
+        keyboard = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz"
+        }
+    
+        clickec = set()
+        lists = [keyboard[i] for i in digits]
+        sol = []
+
+        combinations = list(product(*lists))
+        for combo in combinations:
+            sol.append("".join(list(combo))) 
+
+        return sol
+
