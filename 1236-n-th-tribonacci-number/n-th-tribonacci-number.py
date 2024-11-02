45e2a923d4feaefc323f62dcd657d80b61865e34
Ben N <93800156+benedictnurs@users.noreply.github.com>
2024-11-02 01:34:43 -0700

    Time: 0 ms (100.00%) | Memory: 16.7 MB (12.70%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -2,9 +2,7 @@ class Solution:
     def tribonacci(self, n: int) -> int:
         if n == 0:
             return 0
-        if n == 1:
-            return 1
-        if n == 2:
+        if n == 1 or n == 2:
             return 1
 
         dp = [0] * (n + 1)
