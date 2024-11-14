268a098337eacf1890d6104a3c0c361efb8e8842
Ben N <93800156+benedictnurs@users.noreply.github.com>
2024-11-13 22:46:09 -0800

    Time: 3 ms (69.97%) | Memory: 17 MB (18.60%) - LeetSync

--- 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.py
+++ 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.py
@@ -2,16 +2,14 @@ class Solution:
     def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]:
         hm = {}
         sol = []
-
         for i in range(len(groupSizes)):
             if groupSizes[i] not in hm:
                 hm[groupSizes[i]] = [i]
             else:
                 hm[groupSizes[i]].append(i)
-        
-        for k, num in hm.items():
-            for i in range(0, len(num), k):
-                sol.append(num[i:i+k])
-        
-        return sol
+        print(hm)
 
+        for k,v in hm.items():
+            for i in range(0, (len(v)), k):
+                sol.append(v[i:i+k])
+        return sol
