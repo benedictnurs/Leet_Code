8b7a30cdb078deef9e924751e80ccd6f7d86c2d9
Ben N <93800156+benedictnurs@users.noreply.github.com>
2024-12-17 21:19:22 -0800

    Time: 79 ms (75.74%) | Memory: 23 MB (35.62%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.py
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.py
@@ -2,15 +2,15 @@ class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
         l = 0
         m = 0
-        sol = 0
         odd = 0
+        count = 0
 
         for r in range(len(nums)):
-            if nums[r] % 2 != 0:
+            if nums[r] % 2:
                 odd += 1
             
             while odd > k:
-                if nums[l] % 2 != 0:
+                if nums[l] % 2:
                     odd -= 1
                 l += 1
                 m = l
@@ -18,7 +18,6 @@ class Solution:
             if odd == k:
                 while nums[m] % 2 == 0:
                     m += 1
-                sol += ((m - l) + 1)
+                count += (m-l+1)
 
-        return sol
- 
+        return count
