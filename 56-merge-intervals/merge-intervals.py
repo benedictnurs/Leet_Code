1a94ffc14e1a845165e8e8bd0e80971609da0a20
Ben N <93800156+benedictnurs@users.noreply.github.com>
2025-06-19 03:01:27 -0400

    Time: 10 ms (34.95%) | Memory: 21 MB (43.54%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -4,8 +4,6 @@ class Solution:
         sol = [intervals[0]]
 
         for i in range(1, len(intervals)):
-            print(sol[-1][1], intervals[i][0])
-
             if (sol[-1][1] >= intervals[i][0]):
                 sol[-1][1] = max(sol[-1][1], intervals[i][1])
             else:
