d91400d46b21839fe20bdd147a2563be35e74b78
Ben N <93800156+benedictnurs@users.noreply.github.com>
2025-06-23 01:08:13 -0400

    Time: 3 ms (82.44%) | Memory: 18 MB (24.76%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,53 +1,41 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
+        row = len(board[0])
+        col = len(board)
+
         # Validate col
-        for row in range(len(board)):
+        for c in range(col):
             seen = set()
-            for col in range(len(board[row])):
-                if board[row][col] not in seen:
-                    if board[row][col] != ".":                 
-                        seen.add(board[row][col])
-                else:
-                    return False
+            for r in range(row):
+                if board[r][c] != ".":
+                    if board[r][c] in seen:
+                        return False
+                    seen.add(board[r][c])
 
         # Validate row
-        for row in range(len(board)):
+        for c in range(col):
             seen = set()
-            for col in range(len(board[row])):
-                if board[col][row] not in seen:
-                    if board[col][row] != ".":                 
-                        seen.add(board[col][row])
-                else:
-                    return False
-        
-        hm = {}
-        nb = sum(board,[])
-        count = 0
-        curr = 0
-        pos = 0
-        matrix = 0
-        hm = {}
-        while curr < len(nb):
-            sol_row = []
-            while count < 3:
-                if nb[curr] != ".":
-                    sol_row.append(nb[curr])
-                curr += 1
-                count += 1
-            pos += 1
-            if pos not in hm:
-                hm[pos] = [sol_row]
-            else:
-                hm[pos].append(sol_row)
-            if pos == 3:
-                pos = 0
-            count = 0
+            for r in range(row):
+                if board[c][r] != ".":
+                    if board[c][r] in seen:
+                        return False
+                    seen.add(board[c][r])
 
-        values = list(hm.values())
-        for i in range(len(values)):
-            for j in range(0, len(values[i]), 3):
-                totals = sum(values[i][j:j+3],[])
-                if (len(totals) != len(set(totals))) == True:
+        for square in range(0, 9, 3):
+            tracker = []
+            for c in range(9):
+                seen = []
+                for r in range(square, square + 3):
+                    if board[c][r] != ".":
+                        seen.append(board[c][r])
+                tracker.append(seen)
+
+            for item in range(0, 9, 3):
+                mat = []
+                for i in (tracker[item:item+3]):
+                    mat.extend(i)
+                if len(mat) != len(set(mat)):
                     return False
-                    
+ 
         return True
+        
