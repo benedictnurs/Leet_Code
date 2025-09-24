6c7a971aef7dcaea7d63f4c3d560d6e20ac778f8
Ben N <93800156+benedictnurs@users.noreply.github.com>
2025-09-24 15:46:02 -0700

    Time: 27 ms (46.52%) | Memory: 23.4 MB (54.23%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -7,15 +7,13 @@ class Solution:
         for i in range(len(nums)):
             sol[i] *= prefix
             prefix *= nums[i]
-        print(sol)
 
         postfix = 1
         n = len(nums) - 1
         while n:
-            print(postfix)
             sol[n] *= postfix
             postfix *= nums[n]
             n -= 1
         sol[0] *= postfix
-        
+
         return sol
