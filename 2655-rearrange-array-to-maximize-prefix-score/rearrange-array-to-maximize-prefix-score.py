9fd347006de39acaa7734ee6bad732043967b0d4
Ben N <93800156+benedictnurs@users.noreply.github.com>
2025-10-01 15:08:27 -0700

    Time: 107 ms (31.61%) | Memory: 31.6 MB (65.81%) - LeetSync

--- 2655-rearrange-array-to-maximize-prefix-score/rearrange-array-to-maximize-prefix-score.py
+++ 2655-rearrange-array-to-maximize-prefix-score/rearrange-array-to-maximize-prefix-score.py
@@ -2,15 +2,16 @@ class Solution:
     def maxScore(self, nums: List[int]) -> int:
         nums.sort(reverse=True)
         prefix = nums[0]
-        arr = [nums[0]]
+        # arr = [nums[0]]
         sol = 0
+        if prefix > 0:
+            sol += 1
 
         for i in range(1, len(nums)):
             prefix += nums[i]
-            arr.append(prefix)
-
-        for i in arr:
-            if i > 0:
+            if prefix > 0:
                 sol += 1
+            # arr.append(prefix)
+
 
         return sol
