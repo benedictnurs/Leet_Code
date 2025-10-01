a843bab9562ad0c41fa8fc8ce23ccdd25cc4fab8
Ben N <93800156+benedictnurs@users.noreply.github.com>
2025-10-01 15:08:42 -0700

    Time: 112 ms (22.15%) | Memory: 31.6 MB (65.81%) - LeetSync

--- 2655-rearrange-array-to-maximize-prefix-score/rearrange-array-to-maximize-prefix-score.py
+++ 2655-rearrange-array-to-maximize-prefix-score/rearrange-array-to-maximize-prefix-score.py
@@ -2,7 +2,7 @@ class Solution:
     def maxScore(self, nums: List[int]) -> int:
         nums.sort(reverse=True)
         prefix = nums[0]
-        # arr = [nums[0]]
+
         sol = 0
         if prefix > 0:
             sol += 1
@@ -11,7 +11,5 @@ class Solution:
             prefix += nums[i]
             if prefix > 0:
                 sol += 1
-            # arr.append(prefix)
-
 
         return sol
