54c8a09ea0cfb4ebe08e7ad133567dbaab4ea22f
Ben N <93800156+benedictnurs@users.noreply.github.com>
2025-10-25 14:00:22 -0700

    Time: 0 ms (100.00%) | Memory: 17.6 MB (96.12%) - LeetSync

---
+++ 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.py
@@ -0,0 +1,20 @@
+class Solution:
+    def buildArray(self, target: List[int], n: int) -> List[str]:
+        sol = []
+        if target[-1] < n and len(target) < n:
+            print("X")
+            for i in range(1, target[-1]+1):
+                if i in target:
+                    sol.append("Push")
+                else:
+                    sol.append("Push")
+                    sol.append("Pop")
+            return sol
+
+        for i in range(1, n+1):
+            if i in target:
+                sol.append("Push")
+            else:
+                sol.append("Push")
+                sol.append("Pop")
+        return sol
