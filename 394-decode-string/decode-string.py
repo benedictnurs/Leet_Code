d2baff25e41e43109af28ab5e459faadde65c22e
Ben N <93800156+benedictnurs@users.noreply.github.com>
2025-10-25 13:45:31 -0700

    Time: 0 ms (100.00%) | Memory: 17.7 MB (84.28%) - LeetSync

--- 394-decode-string/decode-string.py
+++ 394-decode-string/decode-string.py
@@ -1,16 +1,19 @@
 class Solution:
     def decodeString(self, s: str) -> str:
         stack = []
+
         for i in s:
             if i != "]":
                 stack.append(i)
-            if i == "]":
+            elif i == "]":
                 curr = ""
-                while stack[-1] and stack[-1] != "[":
+                while stack[-1] != "[":
                     curr = stack.pop() + curr
                 stack.pop()
+
                 num = ""
                 while stack and stack[-1].isdigit():
                     num = stack.pop() + num
-                stack.append(curr * int(num))
+
+                stack.append(curr*int(num))
         return "".join(stack)
