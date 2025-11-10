e2bebab1eba2a73d26ce90f17c0538aa7123be9e
Ben N <93800156+benedictnurs@users.noreply.github.com>
2025-11-10 10:59:45 -0800

    Time: 15 ms (88.81%) | Memory: 18.1 MB (87.90%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -1,17 +1,30 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         sol = 0
-        curr = 0
         n, m = len(grid), len(grid[0])
 
-        def dfs(row, col):
-            if row < 0 or row >= n or col < 0 or col >= m or grid[row][col] != 1:
-                return 0
+        def bfs(row, col):
+            q = deque([(row,col)])
+            directions = [(0,1), (1,0), (0,-1), (-1,0)]
             grid[row][col] = 0
-            return 1 + dfs(row+1,col) + dfs(row-1,col) + dfs(row,col+1) + dfs(row,col-1)
-        
-        for r in range(n):
-            for c in range(m):
-                if grid[r][c] == 1:
-                    sol = max(dfs(r,c), sol)
+            count = 1
+            while q:
+                r, c = q.popleft()
+                for dr, dc in directions:
+                    nr, nc = r+dr, c+dc
+                    if 0 <= nr < n and 0 <= nc < m and grid[nr][nc] == 1:
+                        count += 1
+                        q.append((nr,nc))
+                        grid[nr][nc] = 0
+            nonlocal sol
+            if count > sol:
+                sol = count
+            return count
+            
+        for row in range(n):
+            for col in range(m):
+                if grid[row][col] == 1:
+                    bfs(row,col)
+
+
         return sol
