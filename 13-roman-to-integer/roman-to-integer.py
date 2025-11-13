77dffbfbaecfcb06a51aac8b7bb2303eb850ed64
Ben N <93800156+benedictnurs@users.noreply.github.com>
2025-11-13 15:57:23 -0800

    Time: 19 ms (5.33%) | Memory: 17.8 MB (64.30%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -9,13 +9,11 @@ class Solution:
             "D":500,
             "M":1000,
         }
-        count = 0 
-        s = s.replace("IV","IIII")
-        s = s.replace("IX","VIIII")
-        s = s.replace("XL","XXXX")
-        s = s.replace("XC","LXXXX")
-        s = s.replace("CD","CCCC")
-        s = s.replace("CM","DCCCC")
-        for i in s:
-            count += hm[i]
-        return count
+        count = 0
+        for i in range(len(s)-1):
+            print(s[i], s[i+1])
+            if hm[s[i]] < hm[s[i+1]]:
+                count -= hm[s[i]]
+            else:
+                count += hm[s[i]]
+        return (count +  hm[s[-1]])
