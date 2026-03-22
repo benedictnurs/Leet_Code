93fca84bca7693880b992b6f3178ed96a51bb0e5
Ben N <93800156+benedictnurs@users.noreply.github.com>
2026-03-21 20:07:37 -0700

    Time: 11 ms (13.30%) | Memory: 19.5 MB (14.46%) - LeetSync

--- 2350-find-closest-number-to-zero/find-closest-number-to-zero.py
+++ 2350-find-closest-number-to-zero/find-closest-number-to-zero.py
@@ -1,16 +1,12 @@
 class Solution:
     def findClosestNumber(self, nums: List[int]) -> int:
-        # Greedy problem
-        # Set to inf
-        # Find the minimum
-        # If there is a negative version compare it to 
-        # see if it is the same as the current sol
-        sol = float(inf)
+        hm = {}
 
         for i in nums:
-            if abs(sol) > abs(i):
-                sol = i
-            if abs(sol) == abs(i):
-                sol = max(sol, i)
-                
-        return sol
+            if abs(i) in hm:
+                hm[abs(i)].append(i)
+            else:
+                hm[abs(i)] = [i]
+
+        
+        return (max(hm[min(hm.keys())]))
