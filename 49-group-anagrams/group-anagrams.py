b4a404a6c918cb3e7e79687fb4a166a2aee4d14e
Ben N <93800156+benedictnurs@users.noreply.github.com>
2026-03-22 23:36:33 -0700

    Time: 19 ms (22.30%) | Memory: 22.3 MB (50.38%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,19 +1,8 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        hm = {}
-        
+        hm = defaultdict(list)
+
         for word in strs:
-            key = [0 for _ in range(26)]
+            hm[str(sorted(word))].append(word)
 
-            for letter in word:
-                key[(ord(letter) - ord('a'))] += 1
-            
-            if tuple(key) not in hm:
-                hm[tuple(key)] = [word]
-            else:
-                hm[tuple(key)].append(word)
-        
         return list(hm.values())
-
-
-
