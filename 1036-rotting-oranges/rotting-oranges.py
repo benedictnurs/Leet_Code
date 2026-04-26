bd514c29c35adddaef650a4ba310ab80c8b3bf1f
Ben N <93800156+benedictnurs@users.noreply.github.com>
2026-04-25 23:34:08 -0700

    Time: 3 ms (75.87%) | Memory: 19.4 MB (14.30%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -10,17 +10,16 @@ class Solution:
                 if grid[i][j] == 2:
                     q.append((i,j,0))
 
-        currt = 0
 
         while q:
             r,c,t = q.popleft()
-            currt = max(currt, t)
+            time = max(time, t)
             for dr, dc in directions:
                 nr,nc = dr+r,dc+c
                 if 0 <= nr < n and 0 <= nc < m and grid[nr][nc] == 1:
                     grid[nr][nc] = 2
                     q.append((nr, nc, t+1))
-        time += currt
+
         for i in grid:
             if 1 in i:
                 return -1
